e028d2e12125b341b2ff130f9a281f0f3e5e3d7f
jdurnil <jdurnil@gmail.com>
2018-03-11 14:39:00 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import SearchBar from './components/search_bar';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
+const API_KEY ='AIzaSyDGlYKjUjpsStM-57bz-fk3cAwDI8DfbmY';
+//create new component to produce html
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component  {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term : 'surfboards'}, (videos) => {
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    render() {
+        return(
+        <div>
+            <SearchBar />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos}
+            />
+        </div>
+        );
+    }
+}
+
+//take this components htnl anf put it in dom
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
